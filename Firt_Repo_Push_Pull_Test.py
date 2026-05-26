834efcde87299b549b142414a68cf268c9d20ab1
V Thadeus, Crozen <cvthadeus+deloitte@deloitte.com>
2026-05-26 23:09:53 +0530

    Test the new commits

--- Firt_Repo_Push_Pull_Test.py
+++ Firt_Repo_Push_Pull_Test.py
@@ -1,1 +1,9 @@
-print("Say Hi to Git")
+try:
+    visitor = input("Ask the current session of the day: ").strip().lower()
+except EOFError:
+    visitor = ""
+
+if visitor == "morning":
+    print("Good Morning")
+else:
+    print("Have a nice day")
