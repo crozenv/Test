0f74ee22982d1d39efd24a075345b953125d1304
V Thadeus, Crozen <cvthadeus+deloitte@deloitte.com>
2026-05-29 22:42:45 +0530

    Water

--- Hungry.py
+++ Hungry.py
@@ -4,5 +4,9 @@ if Hungry.lower() == "yes":
     print("You should eat pizza.")
     print("You should eat Burger.")
 else:
-    print("You should drink water.")
+    thursty=input("Are you thirsty? (yes/no) ")
+    if thursty.lower() == "yes": 
+        print("You should drink water.")
+    else:
+        print("Do your work.")
    
